8881198443f92e29b022ba778be2a70416aef627
Andrew Kusnetsov <andrew.kusnetsov@mail.ru>
2025-08-06 20:06:45 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	// Создание нового логгера
+	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime)
+
+	// Создание нового сервера
+	server := server.NewServer(errorLog)
+
+	// Запуск сервера
+	err := http.ListenAndServe(server.HttpServer.Addr, server.HttpServer.Handler)
+
+	// Обработка ошибок
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,129 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+// Путь к корневому index.html
+const indexHTML = "../index.html"
+
+// Обработка корневого эндпоинта /: возвращаем HTML из файла
+func HandleRoot(res http.ResponseWriter, req *http.Request) {
+
+	// Чтение содержимого index.html
+	rootContents, err := readFile(indexHTML)
+
+	if err != nil {
+		http.Error(res, "Failed to open html file:" + err.Error(), http.StatusBadRequest)
+	}
+
+	// Установка типа содержимого text/html и успешного статуса
+	res.Header().Set("Content-Type", "text/html")
+	res.WriteHeader(http.StatusOK)
+
+	// Вывод содержимого index.html в ответ сервера
+	res.Write(rootContents)
+}
+
+// Обработка эндпоинта /upload
+func HandleUpload(res http.ResponseWriter, req *http.Request) {
+
+	// Получение загруженного файла
+	file, _, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(res, "Failed to get file:" + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer file.Close()
+
+	// Чтение содержимого загруженного файла
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(res, "Failed to read file content: " + err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Конвертация содержимого загруженного файла в строку
+	payload := string(fileContent)
+
+	// Конвертация содержимого в текст или код Морзе
+	convertedPayload, err := service.ConvertPayload(payload)
+
+	if err != nil {
+		http.Error(res, "Failed to convert file content:"+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Подготовка названия файла из временной отметки UTC и расширения log
+	fileName := time.Now().UTC().Format("02012006150405") + filepath.Ext(".log")
+
+	// Запись результата конвертации в файл
+	err = writeStringToFile("../"+fileName, convertedPayload)
+
+	if err != nil {
+		http.Error(res, "Failed to record file content: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Установка типа содержимого text/html; charset=utf-8 и успешного статуса
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+    res.WriteHeader(http.StatusOK)
+
+	// Вывод результата конвертации в ответ сервера
+	res.Write([]byte(convertedPayload))
+}
+
+// Чтения файла по его пути
+func readFile(filePath string) ([]byte, error) {
+
+	// Открытие файла на чтение
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0755)
+
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Чтение содержимого файла
+	data, err := io.ReadAll(file)
+
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return nil, err
+	}
+
+	// Возвращаем содержимое файла
+	return data, nil
+}
+
+// Запись в файл строки по его пути
+func writeStringToFile(filePath string, content string) error {
+
+	// Открытие файла на запись
+
+	fmt.Println(filePath)
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC, 0755)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
